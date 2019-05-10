c957e710ec49503f74e97b9463a565f4bafcff74
Damian Bouch <dbouch4009@gmail.com>
2019-05-10 07:48:08 -0400

    ingredient list view works

--- modern_js/src/modern.js
+++ modern_js/src/modern.js
@@ -9,7 +9,9 @@ import Search from './models/Search';
 import {elements, renderLoader,clearLoader,elementStrings} from './views/base';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import Recipe from './models/Recipe';
+import List from './models/List';
 
 /*global state of the app
 - search object
@@ -18,6 +20,9 @@ import Recipe from './models/Recipe';
 - liked recipes
 */
 const state = {};
+
+//List Controller
+
 
 //Recipe Controller
 const controlRecipe = async () => {
@@ -60,7 +65,23 @@ const controlRecipe = async () => {
         }
 
     }
-}
+};
+
+const controlList = () =>{
+    //create new list if there is none
+    if(!state.list){
+        state.list = new List();
+    }
+
+    //add each ingredient to list and UI
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        console.log(item);
+        console.log(state);
+        listView.renderItem(item);
+    });
+};
+
 
 //event handling
 window.addEventListener('hashchange',controlRecipe);
@@ -77,8 +98,10 @@ elements.recipe.addEventListener('click',e => {
         //increase button is clicked
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
+    } else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')){
+        controlList();
     }
-    console.log(state.recipe);
+    //console.log(state.recipe);
 });
 
 //Search Controller
@@ -126,3 +149,5 @@ elements.searchResPages.addEventListener('click',e =>{
         searchView.renderResults(state.search.recipes, goToPage);
     }
 });
+ 
+window.testList = new List();
